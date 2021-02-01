b894e08357198ec35a08145683ccfdb6c5ba404c
Leonardo Omaña <leotangram@gmail.com>
2021-02-01 16:48:07 -0500

    feat(layout login signup): conditional style

---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+const login = () => (
+  <Layout>
+    <h1 className="text-center text-2xl text-white font-light">Login</h1>
+  </Layout>
+)
+
+export default login
---
+++ src/pages/signup.tsx
@@ -0,0 +1,5 @@
+import Layout from '../components/Layout'
+
+const signup = () => <Layout>signup</Layout>
+
+export default signup
