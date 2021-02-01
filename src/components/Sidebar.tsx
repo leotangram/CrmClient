65302964091fc06165d6ef327906df0d3fe188c9
Leonardo Omaña <leotangram@gmail.com>
2021-02-01 16:05:28 -0500

    feat(sidebar): next routing

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link'
+
 const Sidebar = () => (
   <aside className="bg-gray-800 sm:w-1/3 xl:w-1/5 sm:min-h-screen p-5">
     <div>
       <p className="text-white text-2xl font-black">CRM Clientes</p>
     </div>
+    <nav className="mt-5 list-none">
+      <li className="text-white mb-2 block">
+        <Link href="/">Clientes</Link>
+      </li>
+      <li className="text-white mb-2 block">
+        <Link href="/orders">Pedidos</Link>
+      </li>
+    </nav>
   </aside>
 )
 
