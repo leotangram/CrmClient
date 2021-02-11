3b4ccff620be327188c670b979a80512c2b2ff24
Leonardo Omaña <leotangram@gmail.com>
2021-02-11 16:45:01 -0500

    feat(client): add button Delete client

---
+++ src/components/Client.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { IClient } from '../interfaces/IClient'
+
+const Client = ({ id, name, surname, company, email }: IClient) => {
+  const confirmDeleteClient = () => {
+    console.log('Eliminando', id)
+  }
+
+  return (
+    <tr>
+      <td className="border px-4 px-4 py-2">
+        {name} {surname}
+      </td>
+      <td className="border px-4 px-4 py-2">{company}</td>
+      <td className="border px-4 px-4 py-2">{email}</td>
+      <td className="border px-4 px-4 py-2">
+        <button
+          className="flex items-center bg-red-800 py-2 px-4 w-full text-white justify-center rounded text-xs uppercase font-bold"
+          onClick={confirmDeleteClient}
+          type="button"
+        >
+          Eliminar{' '}
+          <svg
+            className="w-4 h-4 ml-2"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
+      </td>
+    </tr>
+  )
+}
+
+export default Client
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { gql, useQuery } from '@apollo/client'
 import { IClient } from '../interfaces/IClient'
 import Layout from '../components/Layout'
+import Client from '../components/Client'
 
 const GET_CLIENTS_SELLER = gql`
   query getClientsSeller {
@@ -41,20 +42,13 @@ const Home = () => {
             <th className="w-1/5 py-2">Nombre</th>
             <th className="w-1/5 py-2">Empresa</th>
             <th className="w-1/5 py-2">Email</th>
+            <th className="w-1/5 py-2">Eliminar</th>
           </tr>
         </thead>
         <tbody className="bg-white">
-          {data?.getClientsSeller?.map(
-            ({ company, email, id, name, surname }: IClient) => (
-              <tr key={id}>
-                <td className="border px-4 px-4 py-2">
-                  {name} {surname}
-                </td>
-                <td className="border px-4 px-4 py-2">{company}</td>
-                <td className="border px-4 px-4 py-2">{email}</td>
-              </tr>
-            )
-          )}
+          {data?.getClientsSeller?.map((userClient: IClient) => (
+            <Client key={userClient.id} {...userClient} />
+          ))}
         </tbody>
       </table>
     </Layout>
